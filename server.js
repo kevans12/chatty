5bfc78f7a2f070db656b40b541cfe59447774caa
Kyle Evans <kylelarryevans@gmail.com>
2016-10-20 18:28:08 -0600

    completed chatty

--- server.js
+++ server.js
@@ -1,1 +1,57 @@
+var express = require('express');
+var app = express();
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+var messages = [];
+
+app.options('/', function(req, res){
+ res.status(200).set({
+   'Content-Type': 'application/json',
+   'Access-Control-Allow-Origin': '*',
+   'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+   'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+   'X-XSS-Protection': '1; mode=block',
+   'X-Frame-Options': 'SAMEORIGIN',
+   'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+   }).send();
+});
+
+app.listen(8000, function(){
+  console.log('Listening on the port 8000');
+});
+
+app.get('/', function( req, res ) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify(messages));
+});
+
+// app.get('/', function(req, res){
+//   res.send(JSON.stringify(messages));
+// });
+
+app.post('/', function( req, res, next ) {
+  console.log(req.body);
+  messages.push({
+    message: req.body.message,
+    time: new Date()
+  });
+
+    res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify(messages));
+});
